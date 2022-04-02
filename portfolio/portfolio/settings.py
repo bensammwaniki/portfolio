98d5b70ed3159fc40d2bdd48d42ca794c32a2afa
bensammwaniki <benson.mwaniki@student.moringaschool.com>
2022-04-02 16:03:34 +0300

    initializind

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -96,6 +96,8 @@ DATABASES = {
         'PASSWORD': 'admin',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
