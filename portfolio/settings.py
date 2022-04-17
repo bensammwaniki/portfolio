f0d5d0f04ce48be6f42e96107a019a7f7a1e37f8
bensammwaniki <benson.mwaniki@student.moringaschool.com>
2022-04-17 15:20:23 +0300

    deploy

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -141,9 +141,10 @@ USE_TZ = True
 
 # Activate b4 deploy
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
