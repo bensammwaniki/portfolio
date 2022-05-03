503ab02c2bbb48e023c23c64320b5f791f6d31f7
Bensam Mwaniki <65493075+bensammwaniki@users.noreply.github.com>
2022-05-03 21:22:06 +0300

    Update settings.py
    Deleted exposed api keys

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -164,6 +164,6 @@ CONTACT_EMAIL = 'bensamgraphics@gmail.com'
 ADMIN_EMAIL = ['bensammwaniki@gmail.com','bensamgraphics@gmail.com']
 
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-SENDGRID_API_KEY = "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"
+SENDGRID_API_KEY = ""
 SENDGRID_SANDBOX_MODE_IN_DEBUG=False
 SENDGRID_ECHO_TO_STDOUT=True
